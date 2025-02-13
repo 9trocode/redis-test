be7114b1de9d4e7a6bcf0f0bbfdeab6e4c237579
Alex Idowu <alexidowu25@gmail.com>
2025-02-13 12:49:56 +0100

    feat: Add go.mod and go.sum files for project dependencies

--- main.go
+++ main.go
@@ -11,13 +11,13 @@ import (
 func main() {
 	// Create a background context for Redis operations.
 	ctx := context.Background()
-      
+
 	// Create a new Redis client.
 	// Adjust the Addr and Password fields as needed.
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "green-dew.agreeable-receipt-beta.svc.pipeops.internal:6379",
 		Password: "pipeops", // set your Redis password here
-		DB:       0,                 // use default DB
+		DB:       0,         // use default DB
 	})
 
 	// Ping the Redis server to test the connection.
